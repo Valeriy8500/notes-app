4980ebed8abff693476f2c4524c8f158ea9480c9
Valeriy <valeriy.brazhnik@sum1.ru>
2024-03-29 17:53:27 +0300

    feat: добавил RichText

--- src/components/note-editor/note-editor.tsx
+++ src/components/note-editor/note-editor.tsx
@@ -5,6 +5,7 @@ import { formatDate } from "../../shared/shared-functions";
 import { INote } from "../../types/types";
 import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
+import RichTextExample from "../rich-text/rich-text";
 import * as S from "./styles";
 
 const initialValue = [
@@ -21,16 +22,22 @@ export const NoteEditor = () => {
   return (
     <>
       {notes.map((item: INote) => {
-        return (
+        return item.isSelected ? (
           <S.NoteEditor key={item.id}>
             <S.DateTime>{formatDate(item.currDateTime)}</S.DateTime>
             <S.Title>{item.content.title}</S.Title>
 
-            <Slate editor={editor} initialValue={initialValue}>
-              <Editable />
-            </Slate>
+            {/* <Slate editor={editor} initialValue={initialValue}>
+              <Editable
+                onKeyDown={event => {
+                  console.log(event.key);
+                }}
+              />
+            </Slate> */}
+
+            <RichTextExample />
           </S.NoteEditor>
-        );
+        ) : null;
       })}
     </>
   );
